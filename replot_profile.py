1df2ed4ebc1922291ff270dbabf19544307e3c25
Neha Joshi <nehajo@sas.upenn.edu>
2024-10-18 16:37:59 -0400

    easier to read plots

---
+++ replot_profile.py
@@ -0,0 +1,48 @@
+import matplotlib.colors as mcolors
+import numpy as np
+from orphics.io import plot_img, Plotter
+import pixell.utils as u
+import sys
+import os
+sys.path.append(os.path.abspath("/home3/nehajo/scripts/"))
+from profiles import errors
+
+names = sys.argv[1:]
+
+plot_mf=True
+
+root = "results/post"
+
+if len(names) == 1:
+    name = names
+    thetas, profile = np.loadtxt(f"{root}/{name}/{name}_opt_profile.txt", unpack=True)
+    _, errs = np.loadtxt(f"{root}/{name}/{name}_opt_profile_errs.txt", unpack=True)
+    _, mf = np.loadtxt(f"{root}/{name}/{name}_mf.txt", unpack=True)
+    _, mf_errs = np.loadtxt(f"{root}/{name}/{name}_mf_errs.txt", unpack=True)
+
+    pl = Plotter(xyscale='linlin', xlabel='$\\theta$ [arcmin]', ylabel='$\\kappa$')
+    pl.add_err(thetas, profile, errs, ls="-", label = "Filtered kappa, meanfield subtracted (optimal)")
+    pl.hline(y=0)
+    pl.done(f"{root}/{name}/{name}_opt_profile_clean_nomf.png")
+
+    if plot_mf: 
+        plmf = Plotter(xyscale='linlin', xlabel='$\\theta$ [arcmin]', ylabel='$\\kappa$')
+        plmf = Plotter(xyscale='linlin', xlabel='$\\theta$ [arcmin]', ylabel='$\\kappa$')
+        plmf.add_err(thetas, profile, errs, ls="-", label = "Filtered kappa, meanfield subtracted (optimal)")
+        plmf.add_err(thetas, mf, mf_errs, ls="-", label = "Mean-field (optimal)")
+        pl.hline(y=0)
+        plmf.done(f"{root}/{name}/{name}_opt_profile_clean.png")
+
+else:
+    pl = Plotter(xyscale='linlin', xlabel='$\\theta$ [arcmin]', ylabel='$\\kappa$')
+    for name in names:
+        thetas, profile = np.loadtxt(f"{root}/{name}/{name}_opt_profile.txt", unpack=True)
+        _, errs = np.loadtxt(f"{root}/{name}/{name}_opt_profile_errs.txt", unpack=True)
+        label = name.replace("_", " ")
+        pl.add_err(thetas, profile, errs, ls="-", label = label)
+    pl.hline(y=0)
+    pl.done(f"{root}/{names[-1]}/{names[-1]}_profile_comp.png")
+
+
+
+
